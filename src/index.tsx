16fba70db2debeda98d14d0be0fef40e6c2f747f
Dániel Sebestyén <dannystyleart@gmail.com>
2024-06-25 14:15:15 +0200

    Add HistoryProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { HistoryContextProvider } from "./history";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <HistoryContextProvider>
+      <App />
+    </HistoryContextProvider>
   </React.StrictMode>
 );
