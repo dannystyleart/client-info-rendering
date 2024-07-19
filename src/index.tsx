4a221bcee1329ec7deac37af022c241d57f2102c
Dániel Sebestyén <dannystyleart@gmail.com>
2024-07-19 10:23:24 +0200

    Add routing to the app

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HistoryContextProvider } from "./history";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -9,7 +10,15 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <HistoryContextProvider>
-      <App />
+      <BrowserRouter basename="/client-info-rendering">
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="v4"element={<App />} />
+          <Route path="v3"element={<App />} />
+          <Route path="v2"element={<App />} />
+          <Route path="v1"element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </HistoryContextProvider>
   </React.StrictMode>
 );
